c0724f06578777e3fd2b1c4f6ce8aece29dd18c8
harshhh28 <harshgajjar2812@gmail.com>
2024-08-11 01:47:09 +0530

    uploading lab01-q1

--- Lab01/server.c
+++ Lab01/server.c
@@ -19,7 +19,7 @@ perror("socket failed");
 exit(EXIT_FAILURE); 
 } 
 // Forcefully attaching socket to the port 8080 - For address reuse
-if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) { 
+if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) { 
 perror("setsockopt"); 
 exit(EXIT_FAILURE); 
 } 
